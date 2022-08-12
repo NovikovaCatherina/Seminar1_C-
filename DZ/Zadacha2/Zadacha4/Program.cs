e43c64a1977b2c8d3ab8c7322005ac7ee909d44b
Katya Novikova <smol-katerina@yandex.ru>
2022-08-12 14:06:20 +0300

    created a folder Zadacha4

---
+++ DZ/Zadacha2/Zadacha4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 3 числа и мы тебе скажем какое из них больше");
+int a, b, c;
+int max = 0;
+Console.WriteLine("Введите свое первое число a ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите свое второе число b ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите свое третье число с ");
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = "); 
+Console.WriteLine(max);
