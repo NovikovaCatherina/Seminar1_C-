5d3374a7b50377d2f733d14627744b682160cc23
Katya Novikova <smol-katerina@yandex.ru>
2022-08-12 14:14:44 +0300

    created a folder Zadacha6

---
+++ DZ/Zadacha6/Program.cs
@@ -0,0 +1,14 @@
+﻿int a;
+Console.WriteLine("Вводим число a ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    if (a % 2 == 0)
+        Console.WriteLine("Число чётное");
+}
+else
+
+{
+
+    Console.WriteLine("число нечётное");
+}
