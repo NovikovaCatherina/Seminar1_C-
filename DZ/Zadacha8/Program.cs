47954ca193df6a50a83baefe30c0f72c729b2429
Katya Novikova <smol-katerina@yandex.ru>
2022-08-12 14:18:48 +0300

    created a folder Zadacha8

---
+++ DZ/Zadacha8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Вводим число от 1 до n ");
+int num = Convert.ToInt32(Console.ReadLine());
+int min = 1;
+
+if (min == 1)
+
+{
+    min = min + 1;
+}
+
+while (min <= num)
+
+{
+    Console.WriteLine(min);
+    min = min + 2;
+
+}
