891dee8e13336045fc8c105d7126cdc9448b9df2
Katya Novikova <smol-katerina@yandex.ru>
2022-08-12 13:58:46 +0300

    created a folder DZ

---
+++ DZ/Zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введи 2 числа и мы тебе скажем какое из них больше");
+int a, b;
+Console.WriteLine("Введи свое первое число a ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи свое второе число b ");
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Число " + a + " больше чем " + b);
+}
+else if (a == b)
+{
+    Console.WriteLine("Числа " + a + " и " + b + " равны");
+}
+else
+{
+    Console.WriteLine("Число " + b + " больше чем " + a);
+}
+
